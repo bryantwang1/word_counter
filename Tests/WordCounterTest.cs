a20aeaa0e417aea85e5302d61f112c1cfa993dc8
Bryant Wang <bryant.m.wang@gmail.com>
2016-12-02 09:16:38 -0800

    Create all 6 tests in Xunit.

--- Tests/WordCounterTest.cs
+++ Tests/WordCounterTest.cs
@@ -1,10 +1,29 @@
 using Xunit;
+using WordCounter.Objects;
 
 namespace WordCounterTest
 {
     public class CountWordAppearancesInInput
     {
+        [Fact]
+        public void StringContainsItself_InputThis_Return1()
+        {
+            string firstWord = "This";
+            RepeatCounter newCounter = new Repeatcounter(firstWord);
+            int result = newCounter.CountRepeats();
+            Assert.Equal(1, result);
+        }
         [Theory]
-        
+        [InlineData("This","This",1)]
+        [InlineData("This","This is a simple sentence",1)]
+        [InlineData("This","this is a simple sentence",1)]
+        [InlineData("This","\"This, is a simple sentence!?\"",1)]
+        [InlineData("This","\"This is a simple sentence is this not?\"",2)]
+        public void CountWordRepeatingInSentence_InputWordThisAndASentence_ReturnAppearancesOfThis(string primeWord, string testSentence, int expectedResult)
+        {
+            RepeatCounter newCounter = new RepeatCounter(primeWord, testSentence)
+            int result = newCounter.CountRepeats();
+            Assert.Equal(expectedResult, result);
+        }
     }
 }
